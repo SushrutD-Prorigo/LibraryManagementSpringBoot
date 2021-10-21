586a2748b959a8462c1f0b189882a787bd0fc10a
SushrutDeore <sushrutdevre@gmail.com>
2021-10-21 14:22:39 +0530

    Spring boot security login with facebook

---
+++ src/main/java/com/SecurityOAuth/Demo/Facebooklogindemospring/FacebookLoginDemoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.SecurityOAuth.Demo.Facebooklogindemospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookLoginDemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookLoginDemoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SecurityOAuth/Demo/Facebooklogindemospring/FacebookLoginDemoSpringApplicationTests.java
@@ -0,0 +1,12 @@
+package com.SecurityOAuth.Demo.Facebooklogindemospring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookLoginDemoSpringApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
